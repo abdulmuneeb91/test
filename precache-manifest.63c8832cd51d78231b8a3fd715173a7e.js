5cb410e6e96ec016106e14f204ede26e8179400d
abdul <muneebabdul2000@gmail.com>
2020-07-14 08:05:15 +0530

    Updates

---
+++ precache-manifest.63c8832cd51d78231b8a3fd715173a7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e6b23ba91abe2893a4af73a64dacf3",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "61c40117fd897956200e",
+    "url": "/test/static/css/main.9186ca35.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/test/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c40117fd897956200e",
+    "url": "/test/static/js/main.8d9ad4eb.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  },
+  {
+    "revision": "5a9fe02acf402b61ea663d37c316b67e",
+    "url": "/test/static/media/profile-image.5a9fe02a.png"
+  }
+]);
